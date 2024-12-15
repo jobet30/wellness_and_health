bbfc3b41bc3e2688c464a01de0343adae30898cd
jobet1130 <jcasquejo56@gmail.com>
2024-12-15 11:46:57 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `wellness_and_health`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
