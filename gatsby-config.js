13340c5b7ebbfd56b2f68e8af29d45d56166b035
jobet1130 <jcasquejo56@gmail.com>
2024-12-15 11:55:28 +0800

    Pre setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
 module.exports = {
   siteMetadata: {
     title: `wellness_and_health`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: ["gatsby-plugin-postcss"],
 };
